9a406bbf27789ce4c6aaff1ccb1bfe3c53c186b3
Saul Augusto Gasca Farrera <saulagf115@gmail.com>
2020-10-01 17:48:16 -0500

    Projected Prepared and Login View with Filter Session Added

---
+++ ResraurantWebApp/ResraurantWebApp/Controllers/AccessController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ResraurantWebApp.Controllers
+{
+    public class AccessController : Controller
+    {
+        // GET: Access
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string user,string password)
+        {
+            try
+            {
+                using (Models.restauranteEntities1 db = new Models.restauranteEntities1())
+                {
+
+                    var oUser = (from r in db.users
+                                 where r.email == user.Trim() && r.password == password.Trim()
+                                 select r).FirstOrDefault();
+
+                    if(oUser == null)
+                    {
+                        ViewBag.Error = "Usuario o Contraseña Inválida";
+                        return View();
+                    }
+
+                    Session["user"] = oUser;
+
+
+                }
+
+
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (Exception ex)
+            {
+
+                ViewBag.Error = ex.Message;
+                return View();
+            }
+        }
+
+
+
+    }
+}
---
+++ ResraurantWebApp/ResraurantWebApp/Filters/CheckSession.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DocumentFormat.OpenXml.Spreadsheet;
+using ResraurantWebApp.Controllers;
+using ResraurantWebApp.Models;
+
+namespace ResraurantWebApp.Filters
+{
+
+    
+    public class CheckSession : ActionFilterAttribute
+    {
+        private users oUser;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+
+
+                oUser = (users)HttpContext.Current.Session["user"];
+                if(oUser == null)
+                {
+                    if(filterContext.Controller is AccessController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Access/Login");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                filterContext.Result = new RedirectResult("~/Access/Login");
+            }
+        }
+    }
+}
